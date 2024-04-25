5821fc69e1118ffdf25fb813bd2af20f4c25dfac
Alysson Almeida <aly.almeida@icloud,com>
2024-04-25 13:51:32 -0300

    exercicio 1144 logical sequence

--- src/Beginner/LogicalSequence1144.java
+++ src/Beginner/LogicalSequence1144.java
@@ -6,17 +6,30 @@ public class LogicalSequence1144 {
 
 	public static void main(String[] args) {
 		
-		int numero;
+		int numero, c1 = 0 , c2 = 0;
+		int soma = 2;
 		
 		Scanner s = new Scanner(System.in);
 		
 		numero =  s.nextInt();
 		
-		for (int i = 1; i<=numero ;i++) {
-			System.out.printf("%d\n", i);
-			System.out.printf("%d\n", i);
-		}
 
+		for (int i = 1; i<=numero ;i++) {
+			if (i==1) {
+				System.out.printf("%d %d %d\n", i, i, i);
+				c1 = i + 1;
+				c2 = i + 1;
+				System.out.printf("%d %d %d\n", i, c1, c2);
+				//soma = soma + 2;
+			} else {
+				c1 = c1+soma;
+				c2 = c1*i;
+				System.out.printf("%d %d %d\n", i, c1, c2);
+				System.out.printf("%d %d %d\n", i, c1+1, c2+1);
+				c1 = c1+1;
+				soma = soma + 2;
+			}
+		}		
 	}
 
 }
